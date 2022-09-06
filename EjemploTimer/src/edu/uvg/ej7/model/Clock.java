31ceaca0383680f3fc8c6fa72fcb093181048920
Moises Alonso <maalonso@uvg.edu.gt>
2022-09-06 17:32:40 -0600

    Attributes of the clock added

--- EjemploTimer/src/edu/uvg/ej7/model/Clock.java
+++ EjemploTimer/src/edu/uvg/ej7/model/Clock.java
@@ -9,4 +9,45 @@ package edu.uvg.ej7.model;
  */
 public class Clock {
 
+	private int hours;
+	private int minutes;
+	private int seconds;
+	/**
+	 * @return the hours
+	 */
+	public int getHours() {
+		return hours;
+	}
+	/**
+	 * @param hours the hours to set
+	 */
+	public void setHours(int hours) {
+		this.hours = hours;
+	}
+	/**
+	 * @return the minutes
+	 */
+	public int getMinutes() {
+		return minutes;
+	}
+	/**
+	 * @param minutes the minutes to set
+	 */
+	public void setMinutes(int minutes) {
+		this.minutes = minutes;
+	}
+	/**
+	 * @return the seconds
+	 */
+	public int getSeconds() {
+		return seconds;
+	}
+	/**
+	 * @param seconds the seconds to set
+	 */
+	public void setSeconds(int seconds) {
+		this.seconds = seconds;
+	}
+	
+	
 }
