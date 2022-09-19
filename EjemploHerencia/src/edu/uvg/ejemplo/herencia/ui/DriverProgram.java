03e3d72d51018688adb67095080b6c09ab9d91e3
Moises Alonso <moises.alonso@telus.com>
2022-09-18 23:04:56 -0600

    Add User interface

---
+++ EjemploHerencia/src/edu/uvg/ejemplo/herencia/ui/DriverProgram.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package edu.uvg.ejemplo.herencia.ui;
+
+import java.util.Scanner;
+
+import edu.uvg.ejemplo.herencia.controller.QuickSort;
+import edu.uvg.ejemplo.herencia.controller.SelectionSort;
+import edu.uvg.ejemplo.herencia.controller.SortingAlgorithm;
+
+/**
+ * @author moises.alonso
+ *
+ */
+public class DriverProgram {
+
+	static int[] items = {2, 5, 10, 3, 50, 15, 30};
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		//Local variables
+		Scanner in = new Scanner(System.in);
+		SortingAlgorithm sortingMethod;
+		
+		System.out.println("Seleccione su método de ordenamiento");
+		System.out.println("1. BubbleSort");
+		System.out.println("2. SelectionSort");
+		System.out.println("3. Quicksort");
+		
+		int option = Integer.parseInt(in.nextLine());
+		
+		switch(option) {
+
+			case 2:{
+				sortingMethod = new SelectionSort(items);
+			}break;
+				
+			case 3:{
+				sortingMethod = new QuickSort(items);
+			}break;
+			
+			default:{
+				sortingMethod = new QuickSort(items);
+			}break;
+		}
+		
+		System.out.println("*** Original array ***");
+		print(items);
+		
+		sortingMethod.sort();
+		
+		System.out.println("*** Array after sort ***");
+		print(items);
+		
+		
+	}
+	
+	static void print(int[] _items) {
+		for (int i = 0; i < items.length; i++) {
+			System.out.println("[ " + i + " ] => " + _items[i]);
+		}
+	}
+
+}
