e800b2e1c8bbaf05443b52746cf3d769f3662f44
Moises Alonso <moises.alonso@telus.com>
2022-11-02 08:23:13 -0600

    Metodos completos

--- Ejercicio6/src/ICancion.java
+++ Ejercicio6/src/ICancion.java
@@ -8,4 +8,23 @@
  */
 public interface ICancion {
 
+	public void setTitle(String _title);
+	
+	public String getTitle();
+	
+	public void setArtist(String _artist);
+	
+	public String getArtist();
+	
+	public void setAlbum(String _album);
+	
+	public String getAlbum();
+	
+	public void setDuration(String _duration);
+	
+	public String getDuration();
+	
+	public void setID(int _id);
+	
+	public int getID();
 }
--- Ejercicio6/src/IIpod_simulator.java
+++ Ejercicio6/src/IIpod_simulator.java
@@ -61,8 +61,81 @@ public interface IIpod_simulator {
 	/***
 	 * Establece el indice Actual
 	 * @param actual_index 
+	 * @exception Exception Cuando el indice esta fuera de rango
 	 */
-	public void setActualIndex(int actual_index);
+	public void setActualIndex(int actual_index) throws Exception;
+	
+	/**
+	 * Este metodo guarda una cancion en listado de favoritos
+	 * @param _song la cancion que se desea guardar
+	 * @exception Exception cuando la lista ya esta llena
+	 */
+	public void addToFavorite(ICancion _song) throws Exception;
+	
+	/***
+	 * Devolvera una cancion especifica del listado general
+	 * @param index el indice en el que se encuentra la cancion
+	 * @return El objeto Cancion
+	 * @exception Exception Cuando el indice esta fuera de rango
+	 */
+	public ICancion selectSpecificSong(int index) throws Exception;
+	
+	/***
+	 * Devolvera una cancion especifica del listado de las favoritas
+	 * @param index el indice en el que se encuentra la cancion
+	 * @return El objeto Cancion
+	 * @exception Exception Cuando el indice esta fuera de rango
+	 */
+	public ICancion selectSpecificFavoriteSong(int index) throws Exception;
+	
+	/**
+	 * Este metodo devuelve el listado completo de canciones
+	 * @return el listado de canciones
+	 */
+	public ICancion[] getAllSongs();
+	
+	/***
+	 * ESte metodo devuelve el estado general del ipod
+	 * @param _isON valor logico que indica si esta encendido o apagado
+	 * @param _isLocked valor logico que indica si esta bloqueado o desbloqueado
+	 * @param _isPlaying valor logico que indica si esta sonando una cancion o no
+ 	 * @param _actualSong valor que indica que cancion esta sonando
+	 * @return Un cadena que contien e el estado de la informacion
+	 */
+	public String getStatus(boolean _isON, boolean _isLocked, boolean _isPlaying, ICancion _actualSong);
+	
+	/***
+	 * Verifica si un indice es valido o no
+	 * @param index el indice que se desea verificar
+	 * @return true si el indice contiene cancion, false de lo cointrario
+	 */
+	public boolean isValidIndex(int index);
+	
+	/***
+	 * Se agrega una cancion para ser instanciada en el metodo y agregada a la lista
+	 * @param _titulo 
+	 * @param _artista
+	 * @param _album
+	 * @param _duracion
+	 * @param _id
+	 * @exception Exception si el listado esta lleno
+	 */
+	public void addSongToList(String _titulo, String _artista, String _album, String _duracion, int _id) throws Exception;
+	
+	/**
+	 * Este metodo elimina una cancion de una posicion determinada
+	 * @param index
+	 * @exception Exception Cuando el indice esta fuera de rango
+	 */
+	public void deleteSongFromList(int index) throws Exception;
+	
+	/**
+	 * Elimina una cancion de la lista de favoritas pero no del listado general
+	 * @param index
+	 * @exception Exception Cuando el indice esta fuera de rango
+	 */
+	public void deleteSongFromTop10(int index) throws Exception;
+	
 	
 	
 }
